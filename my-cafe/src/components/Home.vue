9b373a40ad9a63370db771142b9d0a84b88fcca5
YesYourShin <ehrl827@naver.com>
2021-08-03 16:02:13 +0900

    UI_Framework and book

--- my-cafe/src/components/Home.vue
+++ my-cafe/src/components/Home.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div id="app">
         <h1>Welcome {{userId}}</h1>
         <input v-model="newId" type="text"><button @click="saveUserId">Save</button><br>
@@ -9,6 +9,32 @@
                 <p> {{r.body}}</p>
             </li>
         </ul>
+    </div>
+</template> -->
+
+<template>
+    <div id="app">
+        <h1>Welcome {{userId}}</h1>
+        <v-text-field label="new user id" v-model="newId"></v-text-field>
+        <v-btn @click="saveUserId" elevation="2">Save</v-btn><br>
+        <v-btn @click="updateReviews" color="primary">Update</v-btn>
+        <h1>{{reviewCount}}</h1>
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>Comment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in reviews" :key="item.id">
+                        <td>{{item.email}}</td>
+                        <td>{{item.body}}</td>
+                    </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
     </div>
 </template>
 
